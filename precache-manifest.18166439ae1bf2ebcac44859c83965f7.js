c3f00dc19b52b01a48a1eee2cdaf4e82640fa688
juliannaroen <julianna.roen@gusto.com>
2019-02-03 11:47:51 -0800

    Updates

---
+++ precache-manifest.18166439ae1bf2ebcac44859c83965f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e145b2fcdc7bf22f9fce674ccd0f458",
+    "url": "/static/media/portrait.4e145b2f.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "06dde6632e64b10709b6",
+    "url": "/static/js/main.06dde663.chunk.js"
+  },
+  {
+    "revision": "8e758ad169d171081bae",
+    "url": "/static/js/1.8e758ad1.chunk.js"
+  },
+  {
+    "revision": "06dde6632e64b10709b6",
+    "url": "/static/css/main.2db0960a.chunk.css"
+  },
+  {
+    "revision": "9d3c9a6b700bad62f2dd5ffc5bcecd2b",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/julianna/precache-manifest.2bf6a4dd6ced3d2861ee96a8e20cc957.js"
+  "/precache-manifest.18166439ae1bf2ebcac44859c83965f7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/julianna/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
