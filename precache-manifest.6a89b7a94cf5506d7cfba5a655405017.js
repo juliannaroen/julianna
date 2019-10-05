caac56af1f3578c5903a676a68f5f978cff3afb7
juliannaroen <jsroen@gmail.com>
2019-10-04 20:07:06 -0400

    Updates

---
+++ precache-manifest.6a89b7a94cf5506d7cfba5a655405017.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cba02f1249133cfd0729005f4d089e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23448fb1633a27ea43cd",
+    "url": "/static/css/main.5a569927.chunk.css"
+  },
+  {
+    "revision": "c558ab20a124d7a5ce29",
+    "url": "/static/js/2.0ecf87e6.chunk.js"
+  },
+  {
+    "revision": "23448fb1633a27ea43cd",
+    "url": "/static/js/main.802be44e.chunk.js"
+  },
+  {
+    "revision": "16516f5cd57e10e4d9b0",
+    "url": "/static/js/runtime-main.fd5d6083.js"
+  },
+  {
+    "revision": "682c310adb50c3b21e21a8de9871f956",
+    "url": "/static/media/polaroid-portrait-sm.682c310a.png"
+  }
+]);
